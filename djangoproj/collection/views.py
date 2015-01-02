404075a4c3b9ad6b7a6031903cedb312a70ef2dd
Vinay <vinaynair89@gmail.com>
2015-01-02 04:31:55 -0800

    template changes with index page navigations

--- djangoproj/collection/views.py
+++ djangoproj/collection/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 
+def home(request):
+	return render_to_response('index.html')
+
+def about(request):
+	return render_to_response('about.html')
+
+def contact(request):
+	return render_to_response('contact.html')
 # Create your views here.
--- djangoproj/djangoproj/urls.py
+++ djangoproj/djangoproj/urls.py
@@ -1,12 +1,17 @@
 from django.conf.urls import patterns, include, url
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
+from django.views.generic import TemplateView
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'djangoproj.views.home', name='home'),
+    url(r'^$', 'collection.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^about/', 'collection.views.about', name='about'),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'),name='about'),
+    url(r'^contact-me/$', 'collection.views.contact', name='contact'),
+    url(r'^admin/$', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
