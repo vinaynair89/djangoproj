8167783f1b42ed2364ea285ef7d1426f864e8e34
Vinay <vinaynair89@gmail.com>
2015-01-02 06:13:07 -0800

    static pages before creating models

--- djangoproj/collection/views.py
+++ djangoproj/collection/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, render_to_response
 
 def home(request):
-	return render_to_response('index.html')
+	number=6
+	return render_to_response('index.html',{
+		'numb':number,
+		})
 
 def about(request):
 	return render_to_response('about.html')
